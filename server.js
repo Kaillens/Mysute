d2ccafd4ec142b18e7aeb5266d44ff11f6cd11ca
Kaillensopoulos <arnaud.scieur@gmail.com>
2018-10-10 12:07:06 +0200

    db get

--- server.js
+++ server.js
@@ -23,15 +23,7 @@ next();
 app.use(express.static(__dirname + '/dist/MonSite'));
 
 app.get('/*', async function(req,res) {
-if(req.url === '/GetArticle'){
-  let Origin = parseInt(req.query.Number);
-    let ArticleList = await modele.find({}, {'_id.$oid':0}).sort({'Article.Date': -1});
-    res.json(ArticleList.slice(Origin,Origin+2));
-}
-else{
-console.log('get');    
 res.sendFile(path.join(__dirname+'/dist/MonSite/index.html'));
-}
 });
 
 const article = new mongoose.Schema({
@@ -51,6 +43,7 @@ const article = new mongoose.Schema({
 
 const modele = mongoose.model('article', article);
 app.get("/GetArticle", async function (req,res,){
+  res.end(JSON.stringify('Votre a message a été envoyé'));
   console.log(req.url);
   console.log('dvds')
     let Origin = parseInt(req.query.Number);
